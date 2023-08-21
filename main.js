0e228106a263d50994731f7e14aee71bf543e6b8
v.semenenko <v.semenenko@advisability.com>
2023-08-22 02:04:31 +0300

    сверстал и адаптировал страницы Блог, Новости, Обо мне

--- main.js
+++ main.js
@@ -22,4 +22,9 @@ const pug = require('pug');
 
 // let html = pug.renderFile('layout.pug');
 // fs.writeFileSync('index.html', pug.renderFile('layout/pages/layout.pug', { basedir: 'layout' }));
-fs.writeFileSync('index.html', pug.renderFile('layout/pages/layout.pug'));
+fs.writeFileSync('index.html', pug.renderFile('layout/pages/index.pug', { title: 'ИП Лагутенкова' }));
+fs.writeFileSync('about.html', pug.renderFile('layout/pages/about.pug', { title: 'Обо мне' }));
+fs.writeFileSync('blog.html', pug.renderFile('layout/pages/blog.pug', { title: 'Блог' }));
+fs.writeFileSync('news.html', pug.renderFile('layout/pages/news.pug', { title: 'Новости' }));
+// fs.writeFileSync('index.html', pug.renderFile('layout/pages/index.pug'));
+// fs.writeFileSync('about.html', pug.renderFile('layout/pages/about.pug'));
