dfb0536b3ddc3e0bf9347b80c6ba8db11b3ac1f3
v.semenenko <v.semenenko@advisability.com>
2023-08-23 12:05:45 +0300

    Сверстал и адаптировал страницы для публикаций новостей и блога, подключил миксины для последних постов новостей и блога

--- main.js
+++ main.js
@@ -21,10 +21,12 @@ const pug = require('pug');
 //     }
 
 // let html = pug.renderFile('layout.pug');
-// fs.writeFileSync('index.html', pug.renderFile('layout/pages/layout.pug', { basedir: 'layout' }));
+// fs.writeFileSync('index.html', pug.renderFile('layout/pages/layout.pug', { basedir: 'layout' }));т
 fs.writeFileSync('index.html', pug.renderFile('layout/pages/index.pug', { title: 'ИП Лагутенкова' }));
 fs.writeFileSync('about.html', pug.renderFile('layout/pages/about.pug', { title: 'Обо мне' }));
 fs.writeFileSync('blog.html', pug.renderFile('layout/pages/blog.pug', { title: 'Блог' }));
 fs.writeFileSync('news.html', pug.renderFile('layout/pages/news.pug', { title: 'Новости' }));
+fs.writeFileSync('blog-publication.html', pug.renderFile('layout/pages/blog-publication.pug', { title: 'Обзор сайта АКЦ' }));
+fs.writeFileSync('news-publication.html', pug.renderFile('layout/pages/news-publication.pug', { title: '47% бетторов ставят на победу «Сочи» над «Пари НН» в РПЛ' }));
 // fs.writeFileSync('index.html', pug.renderFile('layout/pages/index.pug'));
 // fs.writeFileSync('about.html', pug.renderFile('layout/pages/about.pug'));
